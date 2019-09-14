2f89089abfbe02de6e08d2f3082e81ecaadfdfbc
Enlil Odisho <egodisho@ucsd.edu>
2019-09-14 06:01:53 +0100

    Require access codes to update fans.

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ app.get('/api/fans/:remote_id/:option', (req, res) => {
 
 app.put('/api/fans/:remote_id/:option', (req, res) => {
     // Check if correct query data passed.
+    if (!('access_code' in req.query)) {
+        res.status(400).json({success:0,msg:'Missing access code.'});
+    }
     if (!('value' in req.query)) {
         res.status(400).json({success:0,msg:'Missing new value.'});
         return;
@@ -48,6 +51,11 @@ app.put('/api/fans/:remote_id/:option', (req, res) => {
         res.status(404).json({success:0,msg:'Fan with this remote id does not exist.'});
         return;
     }
+    // Check if fan access code is correct.
+    if (fan.access_code != req.query.access_code) {
+        res.status(403).json({success:0,msg:'Invalid fan access code provided.'});
+        return;
+    }
     // Check if option is invalid.
     if (!(req.params.option in fan)) {
         res.status(404).json({success:0,msg:'Option does not exist for fan.'});
@@ -55,35 +63,46 @@ app.put('/api/fans/:remote_id/:option', (req, res) => {
     }
     // Check if new value provided for option.
     if (fan[req.params.option].toString() != req.query.value) {
+        // Contains option and arguments to run python fan remote script.
         var python_options = {
             mode: 'text',
             pythonPath: 'python3',
             args: [fan.remote_id]
         };
-        function run_fan_remote(successCallback) {
+        // Run FanRemote python script with python_options
+        function run_fan_remote(callback) {
             PythonShell.run('FanRemote.py', python_options, function(err, results) {
-                if (err) {
-                    res.status(500).json({success:0,msg:'Error with fan communication module.'});
-                }
-                res.json({success:1});
-                if (typeof successCallback === 'function') {
-                    successCallback();
+                if (typeof callback === 'function') {
+                    if (err) {
+                        callback(false);
+                    } else {
+                        callback(true);
+                    }
                 }
             });
         }
+
+        // Process put request received.
         switch(req.params.option) {
             case 'light_on':
                 // Toggle fan light.
                 python_options.args.push('light');
-                run_fan_remote(() => {
-                    // Update fan option.
-                    var setLightOn = (req.query.value.toLowerCase() == 'true');
-                    fan[req.params.option] = setLightOn;
+                run_fan_remote((success) => {
+                    if (success) {
+                        // Update fan option.
+                        var is_light_on = (req.query.value.toLowerCase() == 'true');
+                        fan['light_on'] = is_light_on;
+                        res.json({success:1});
+                    } else {
+                        res.status(500).json({success:0,msg:'Error with fan communication python script.'});
+                    }
                 });
                 break;
             default:
                 fan[req.params.option] = req.query.value;
+                res.json({success:1});
         }
+
     } else {
         res.status(400).json({success:0,msg:'Option not changed.'});
     }
@@ -98,7 +117,7 @@ function startServer() {
 }
 
 console.log('Harbor Breeze Ceiling Fan Hub');
-console.log('Turn off all fans, then press any key to start hub.');
+console.log('Make sure all fans are at default state (as listed in fans.json), then press any key to start hub.');
 process.stdin.setRawMode(true);
 process.stdin.resume();
 process.stdin.once('data', () => {
