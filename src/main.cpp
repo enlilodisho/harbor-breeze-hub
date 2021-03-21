af7b676e28f65bad129bcefa4ea3833eeddb2d1a
Enlil Odisho <github@enlilodisho.com>
2021-03-21 06:43:32 +0000

    added version

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <HarborBreezeHubConfig.h>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Harbor Breeze Hub - v" << HARBOR_BREEZE_HUB_VERSION_MAJOR
+              << "." << HARBOR_BREEZE_HUB_VERSION_MINOR
+              << "." << HARBOR_BREEZE_HUB_VERSION_PATCH << std::endl;
     return 0;
 }
